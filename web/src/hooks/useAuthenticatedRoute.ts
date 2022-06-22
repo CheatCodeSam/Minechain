028eb684bfd7785756300451d891591977751f85
Carson Weeks <CarsonWeeks@mail.com>
2022-06-22 15:35:02 -0700

    Fixed Auth Hook

--- web/src/hooks/useAuthenticatedRoute.ts
+++ web/src/hooks/useAuthenticatedRoute.ts
@@ -6,18 +6,15 @@ import { useNavigate } from "react-router-dom"
 import { AuthStatus } from "../features/auth/auth.types"
 import { State } from "../store"
 
-const useAuthenticatedRoute = () => {
+const useAuthenticatedRoute = (callback = "/") => {
   const navigate = useNavigate()
   const { authStatus } = useSelector((state: State) => state.auth)
-  const { user } = useSelector((state: State) => state.auth)
 
   useEffect(() => {
     if (authStatus == AuthStatus.AnonymousUser) {
-      navigate("/")
+      navigate(callback)
     }
-  }, [authStatus, navigate])
-
-  return user
+  }, [authStatus, callback, navigate])
 }
 
 export default useAuthenticatedRoute
