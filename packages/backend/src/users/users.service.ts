96fdf7c3939701d7155a88da86b8fb4cc3a14088
sophia.sandoval <sandovalm.sophia@gmail.com>
2022-09-19 13:34:14 -0700

    finished getNFT function

---
+++ packages/backend/src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { EvmChain } from "@moralisweb3/evm-utils"
+import Moralis from "moralis"
+
+import { Injectable } from "@nestjs/common"
+
+const gateway = "https://ipfs.moralis.io:2053/ipfs/"
+
+@Injectable()
+export class UsersService {
+  async getNfts(publicAddress: string) {
+    const nfts = await Moralis.EvmApi.account.getNFTs({
+      chain: EvmChain.RINKEBY,
+      address: publicAddress
+    })
+    const images: string[] = []
+    nfts.result.forEach(async (nft) => {
+      const image = nft.format().metadata.image as string
+      if (image.startsWith("http")) {
+        images.push(image)
+      } else if (image.startsWith("ipfs")) {
+        const ipfsID = image.replace("ipfs://", "")
+        const ipfsGateway = gateway + ipfsID
+        images.push(ipfsGateway)
+      }
+    })
+    console.log(images)
+
+    return images
+  }
+}
