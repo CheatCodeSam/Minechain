5e813ebacb0603cd406702bc42584c709ca22343
Carson Weeks <mail@carsonweeks.com>
2022-09-24 22:51:55 -0700

    Made express loader for future feature.

--- packages/backend/src/admin/express.loader.ts
+++ packages/backend/src/admin/express.loader.ts
@@ -5,11 +5,10 @@ import AdminJS from "adminjs"
 import { Injectable } from "@nestjs/common"
 import { AbstractHttpAdapter } from "@nestjs/core"
 
+// Just setting this up for later.
 @Injectable()
 export class ExpressCustomLoader extends AbstractLoader {
   public register(admin: AdminJS, httpAdapter: AbstractHttpAdapter, options: AdminModuleOptions) {
-    console.log("Custom loader")
-    console.log("Custom loader")
     new ExpressLoader().register(admin, httpAdapter, options)
   }
 }
