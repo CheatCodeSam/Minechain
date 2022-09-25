786154ebda4394a42807d249987364766d91cbad
Carson Weeks <mail@carsonweeks.com>
2022-09-25 14:40:39 -0700

    Using Custom Loader

--- packages/backend/src/admin/admin.module.ts
+++ packages/backend/src/admin/admin.module.ts
@@ -9,18 +9,6 @@ import { Token } from "../blockchain/token.entity"
 import { User } from "../users/entities/user.entity"
 import { ExpressCustomLoader } from "./express.loader"
 
-const DEFAULT_ADMIN = {
-  email: "admin@example.com",
-  password: "password"
-}
-
-const authenticate = async (email: string, password: string) => {
-  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-    return Promise.resolve(DEFAULT_ADMIN)
-  }
-  return null
-}
-
 AdminJS.registerAdapter({
   Resource: AdminJSTypeorm.Resource,
   Database: AdminJSTypeorm.Database
@@ -69,16 +57,6 @@ AdminJS.registerAdapter({
             Token,
             Session
           ]
-        },
-        auth: {
-          authenticate,
-          cookieName: "adminjs",
-          cookiePassword: "secret"
-        },
-        sessionOptions: {
-          resave: true,
-          saveUninitialized: true,
-          secret: "secret"
         }
       })
     })
