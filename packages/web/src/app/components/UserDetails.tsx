f31d6474f333d76d94a203b756896e64f170f11a
Carson Weeks <mail@carsonweeks.com>
2022-10-03 12:48:55 -0700

    added admin button to super users in user details.

--- packages/web/src/app/components/UserDetails.tsx
+++ packages/web/src/app/components/UserDetails.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { CogIcon, UserIcon } from "@heroicons/react/24/outline"
+import { BuildingLibraryIcon, CogIcon, UserIcon } from "@heroicons/react/24/outline"
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid"
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon"
 import { useDispatch, useSelector } from "react-redux"
@@ -30,6 +30,12 @@ const UserHeader = () => {
             <CogIcon className="h-5 w-5" />
             Settings
           </Link>
+          {user.isSuperUser && (
+            <a href="/admin">
+              <BuildingLibraryIcon className="h-5 w-5" />
+              Admin
+            </a>
+          )}
           <button onClick={() => dispatch(logout())}>
             <ArrowLeftOnRectangleIcon className="h-5 w-5" /> Logout
           </button>
