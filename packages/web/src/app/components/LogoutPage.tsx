0fd8a34587492f3ca7dd8b666de1fce2e896a9c9
sophia.sandoval <sandovalm.sophia@gmail.com>
2022-10-11 15:42:01 -0700

    rewrote the logout page to redirect to login using state

--- packages/web/src/app/components/LogoutPage.tsx
+++ packages/web/src/app/components/LogoutPage.tsx
@@ -1,14 +1,26 @@
 
-import { Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { Navigate, useLocation } from "react-router-dom"
+import { AuthStatus } from "../features/auth/auth.types"
+import { State } from "../store"
+
 
 const LogoutPage = () => {
-return(
+    const { authStatus } = useSelector((state: State) => state.auth)
+  const state = useLocation().state as { redirect?: string }
 
-    <Navigate to= "/login" replace = {true} />
-
+  if (authStatus === AuthStatus.AnonymousUser) {
+    return <Navigate to={state?.redirect || "/login"} />
+  }
+  else {
+    return (
+      <div>
+        Dedicated Logout Page
+      </div>
     )
-
+  }
 }
+
   
   export default LogoutPage
   
