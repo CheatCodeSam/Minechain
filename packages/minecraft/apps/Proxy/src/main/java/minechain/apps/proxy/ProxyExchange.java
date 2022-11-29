5f3844a1531cacdca50fc2a62cb5dff41c497672
Carson Weeks <mail@carsonweeks.com>
2022-11-28 18:35:12 -0800

    Proxy moves player to other server

--- packages/minecraft/apps/Proxy/src/main/java/minechain/apps/proxy/ProxyExchange.java
+++ packages/minecraft/apps/Proxy/src/main/java/minechain/apps/proxy/ProxyExchange.java
@@ -5,13 +5,17 @@ import com.rabbitmq.client.Delivery;
 import com.velocitypowered.api.proxy.ProxyServer;
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
+import java.util.UUID;
 import minechain.libs.rabbit.Exchange;
 import minechain.libs.rabbit.Route;
 
 public class ProxyExchange extends Exchange {
 
+  private ProxyServer server;
+
   public ProxyExchange(ProxyServer server) {
     super("proxy", "direct", true);
+    this.server = server;
   }
 
   @Route(routingKey = "transfer")
@@ -20,6 +24,13 @@ public class ProxyExchange extends Exchange {
     var message = new String(delivery.getBody(), "UTF-8");
     var gson = new Gson();
     Map map = gson.fromJson(message, Map.class);
-    System.out.println(map.get("uuid"));
+    var uuid = UUID.fromString(map.get("uuid").toString());
+    var player = this.server.getPlayer(uuid);
+    var mc = this.server.getServer("minechain");
+    if (mc.isPresent() && player.isPresent()) {
+      var p = player.get();
+      var serv = mc.get();
+      p.createConnectionRequest(serv).connect();
+    }
   }
 }
