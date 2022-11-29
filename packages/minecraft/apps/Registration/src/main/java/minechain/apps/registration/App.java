47fa2433ffa9dd69bca244ed231fb9cdb21f71e7
Carson Weeks <mail@carsonweeks.com>
2022-11-28 21:05:11 -0800

    Kicking player if afk in reg

--- packages/minecraft/apps/Registration/src/main/java/minechain/apps/registration/App.java
+++ packages/minecraft/apps/Registration/src/main/java/minechain/apps/registration/App.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import minechain.libs.rabbit.Rabbit;
+import net.kyori.adventure.text.Component;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -41,11 +42,6 @@ public class App extends JavaPlugin implements Listener {
     Map<String, String> stringMap = new LinkedHashMap<>();
     stringMap.put("uuid", player.getUniqueId().toString());
     Rabbit.getInstance().publish("minecraft", "playerJoin", gson.toJson(stringMap));
-  }
-
-  @EventHandler
-  public void onPlayerSpawn(PlayerRespawnEvent e) {
-    var player = e.getPlayer();
 
     new BukkitRunnable() {
       @Override
@@ -56,5 +52,13 @@ public class App extends JavaPlugin implements Listener {
       }
     }
       .runTaskLater(this, 30);
+
+    new BukkitRunnable() {
+      @Override
+      public void run() {
+        player.kick(Component.text("Kicked for being AFK"));
+      }
+    }
+      .runTaskLater(this, 20 * 60 * 5);
   }
 }
