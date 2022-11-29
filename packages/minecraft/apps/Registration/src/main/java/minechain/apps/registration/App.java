e3c968f0fe89068e3bf03f73d3eaa4a8653359ea
Carson Weeks <mail@carsonweeks.com>
2022-11-28 17:16:45 -0800

    Registration server listening to rabbit

--- packages/minecraft/apps/Registration/src/main/java/minechain/apps/registration/App.java
+++ packages/minecraft/apps/Registration/src/main/java/minechain/apps/registration/App.java
@@ -1,9 +1,14 @@
 package minechain.apps.registration;
 
+import com.google.gson.Gson;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import minechain.libs.rabbit.Rabbit;
 import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,11 +21,26 @@ public class App extends JavaPlugin implements Listener {
   @Override
   public void onEnable() {
     getServer().getPluginManager().registerEvents(this, this);
+    Rabbit.getInstance().registerExchange(new RegistrationExchange(this));
+  }
+
+  @Override
+  public void onDisable() {
+    Rabbit.getInstance().close();
   }
 
   @EventHandler
   public void onPlayerMove(PlayerMoveEvent e) {
     e.setCancelled(true);
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent playerJoinEvent) {
+    Player player = playerJoinEvent.getPlayer();
+    Gson gson = new Gson();
+    Map<String, String> stringMap = new LinkedHashMap<>();
+    stringMap.put("uuid", player.getUniqueId().toString());
+    Rabbit.getInstance().publish("minecraft", "playerJoin", gson.toJson(stringMap));
   }
 
   @EventHandler
---
+++ packages/minecraft/apps/Registration/src/main/java/minechain/apps/registration/RegistrationExchange.java
@@ -0,0 +1,59 @@
+package minechain.apps.registration;
+
+import com.google.gson.Gson;
+import com.rabbitmq.client.Delivery;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.UUID;
+import minechain.libs.rabbit.Exchange;
+import minechain.libs.rabbit.Route;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+
+public class RegistrationExchange extends Exchange {
+
+  private App instance;
+
+  public RegistrationExchange(App instance) {
+    super("registration", "direct", true);
+    this.instance = instance;
+  }
+
+  @Route(routingKey = "registerToken")
+  public void registerToken(String consumerTag, Delivery delivery)
+    throws UnsupportedEncodingException {
+    var message = new String(delivery.getBody(), "UTF-8");
+    var gson = new Gson();
+
+    Map map = gson.fromJson(message, Map.class);
+
+    var msg = new TextComponent("Click here to register your account.");
+
+    msg.setClickEvent(
+      new ClickEvent(
+        ClickEvent.Action.OPEN_URL,
+        "http://localhost:4200/register/" + map.get("registerToken")
+      )
+    );
+    msg.setColor(ChatColor.RED);
+    msg.setUnderlined(true);
+
+    var player = Bukkit.getServer().getPlayer(UUID.fromString(map.get("uuid").toString()));
+    System.out.println(player.displayName().toString());
+    if (player != null) player.sendMessage(msg);
+  }
+
+  @Route(routingKey = "authorizeJoin")
+  public void success(String consumerTag, Delivery delivery) throws UnsupportedEncodingException {
+    String message = new String(delivery.getBody(), "UTF-8");
+    Gson gson = new Gson();
+    Map<String, Object> map = gson.fromJson(message, Map.class);
+    String msg = map.get("publicAddress").toString();
+
+    var player = Bukkit.getPlayer(UUID.fromString(map.get("mojangId").toString()));
+
+    Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(msg));
+  }
+}
