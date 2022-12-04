3fb41802a525af489c23a1d1e79b7c24fabe13d0
Carson Weeks <mail@carsonweeks.com>
2022-12-03 19:41:54 -0800

    Deployed smart contract.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,11 +1,14 @@
 import "@nomicfoundation/hardhat-chai-matchers"
 import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
 import "@typechain/hardhat"
+import service from "dotenv"
 import { HardhatUserConfig } from "hardhat/config"
 import "solidity-coverage"
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+service.config()
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API } = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
@@ -17,9 +20,16 @@ const config: HardhatUserConfig = {
     cache: "../../dist/hardhat/cache",
     artifacts: "../../dist/hardhat/artifacts"
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API
+  },
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   }
 }
