0f2b613c6b169411a6945c6f10b1a7b8f4f372b8
Carson Weeks <mail@carsonweeks.com>
2023-04-07 00:29:00 -0700

    Add Minechain smart contract with basic functions for managing tokens and rent in Minechain.sol

---
+++ packages/hardhat/contracts/Minechain.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Minechain {
+    // numberOfTokens = 1024
+    
+    // mapping(address => Account) public accounts;
+
+    // tokenHolderOnly, ValidTokenOnly
+    // setPriceOf(tokenid, price)
+
+    // validTokenOnly, nonTokenHolderOnly
+    // buy(tokenId, newPrice)
+
+    // depositRent()
+    // withdrawRent()
+
+    // ownerOnly
+    // collectRent(tokenId)
+}
