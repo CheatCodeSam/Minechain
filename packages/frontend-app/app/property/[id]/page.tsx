c708493b7ae7ed37ce8552eb5ca3745a667958de
Carson Weeks <mail@carsonweeks.com>
2023-05-29 20:35:31 -0700

    Add link to owner address in FindOne page component

--- packages/frontend-app/app/property/[id]/page.tsx
+++ packages/frontend-app/app/property/[id]/page.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
 
 async function getProperty(id: string) {
-    const res = await fetch(`http://localhost:3333/api/property/${id}`, { cache: 'no-store' });
-    return res.json();
-  }
- 
-export default async function FindOne(props: { params: {id: string} }) {
- const property = await getProperty(props.params.id) 
-  return <div className="">
-    <div className="">{property.ownerAddress}</div>
-    <div className="">{property.price}</div>
-    <button className='underline'>Buy</button>
-  </div>;
+  const res = await fetch(`http://localhost:3333/api/property/${id}`, {
+    cache: 'no-store',
+  })
+  return res.json()
+}
+
+export default async function FindOne(props: { params: { id: string } }) {
+  const property = await getProperty(props.params.id)
+  return (
+    <div className="">
+      <div className="">
+        <Link href={`/user/${property.ownerAddress}`}>
+          {property.ownerAddress}
+        </Link>
+      </div>
+      <div className="">{property.price}</div>
+      <button className="underline">Buy</button>
+    </div>
+  )
 }
