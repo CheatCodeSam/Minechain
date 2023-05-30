fa4cac7618339aa65a6c2c4ecbd9c07600542af1
Carson Weeks <mail@carsonweeks.com>
2023-05-30 13:21:14 -0700

    Refactor JwtStrategy to use ConfigService and UserService for improved modularity and security

--- packages/backend/src/auth/strategy/jwt.strategy.ts
+++ packages/backend/src/auth/strategy/jwt.strategy.ts
@@ -1,21 +1,24 @@
-
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { UserService } from '../../user/user.service'
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
-  constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "secret",
-    });
+      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+    })
   }
 
   async validate(payload: any) {
-    console.log(payload)
-    return { userId: payload.sub };
+    const id = payload.sub
+    return this.userService.findOne({ id })
   }
 }
