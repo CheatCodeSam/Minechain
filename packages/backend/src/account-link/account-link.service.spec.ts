cd923cc17df331c51a3237d07ba3882553c0c2ab
Carson Weeks <mail@carsonweeks.com>
2023-06-12 14:34:12 -0700

    Add test case for generating registration JWT in AccountLinkService spec

--- packages/backend/src/account-link/account-link.service.spec.ts
+++ packages/backend/src/account-link/account-link.service.spec.ts
@@ -4,13 +4,15 @@ import { AccountLinkService } from './account-link.service'
 import { DeepMocked, createMock } from '@golevelup/ts-jest'
 import { UserService } from '../user/user.service'
 import { User } from '../user/user.entity'
+import { ConfigService } from '@nestjs/config'
 
 const createUser = (user: Partial<User> = {}) => {
   const retVal = new User()
   retVal.id = user.id || 1
   retVal.isActive = user.isActive || true
   retVal.nonce = user.nonce || 'randomNonce'
-  retVal.publicAddress = user.publicAddress || '0x2061dd3a9f09186b5CD82436467dDB79dC737227'
+  retVal.publicAddress =
+    user.publicAddress || '0x2061dd3a9f09186b5CD82436467dDB79dC737227'
   retVal.mojangId = user.mojangId || 'd772f296-60a0-4917-bf8f-7f33ffed41d9'
   retVal.lastKnownRegion = user.lastKnownRegion || '1'
   retVal.dateJoined = user.dateJoined || new Date()
@@ -27,6 +29,7 @@ const createUser = (user: Partial<User> = {}) => {
 describe('CatsController', () => {
   let accountLinkService: AccountLinkService
   let userService: DeepMocked<UserService>
+  let configService: DeepMocked<ConfigService>
   let user: User
 
   beforeEach(async () => {
@@ -38,14 +41,15 @@ describe('CatsController', () => {
 
     accountLinkService = moduleRef.get<AccountLinkService>(AccountLinkService)
     userService = moduleRef.get(UserService)
+    configService = moduleRef.get(ConfigService)
     user = createUser({})
   })
 
   describe('accountLinkService', () => {
-
     it('should call unlink account', async () => {
       const mockUser = createUser()
-      const unlinkMinecraftAccountFunction = userService.unlinkMinecraftAccount.mockResolvedValue(mockUser)
+      const unlinkMinecraftAccountFunction =
+        userService.unlinkMinecraftAccount.mockResolvedValue(mockUser)
 
       const unlinkedUser = await accountLinkService.unlinkAccount(user)
 
@@ -53,5 +57,16 @@ describe('CatsController', () => {
       expect(unlinkMinecraftAccountFunction).toBeCalledWith(1)
     })
 
+    it('should create a JWT', async () => {
+      configService.get.mockReturnValueOnce('secret')
+      const jwtRegex = /^[\w-]+\.[\w-]+\.[\w-]+$/g
+
+      const jwt = await accountLinkService.generateRegistrationToken(
+        user.mojangId
+      )
+
+      expect(jwt.token).toMatch(jwtRegex)
+      expect(jwt.uuid).toEqual(user.mojangId)
+    })
   })
 })
